d4205d15e2502e8b70058ddf32433ea372cbb9f0
earthrobot <syley-20@yandex.ru>
2020-11-26 10:12:15 +0400

    add migration database domains

---
+++ database/migrations/2020_11_24_111529_create_domains_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDomainsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('domains', function (Blueprint $table) {
+            $table->id();
+            $table->name();
+            $table->updated_at();
+            $table->created_at();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('domains');
+    }
+}
